2dfaa3cf61c05e571e9af58a9aa235a5bc1615d6
giorgiagiro <61548076+giorgiagiro@users.noreply.github.com>
2022-11-09 16:00:46 +0200

    added build

---
+++ build/old/easyMovies/precache-manifest.110ef97c3ec7e02dd0788eac5c0ad137.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24328c15e4500f2a9f4deead2e1395de",
+    "url": "/easyMovies/index.html"
+  },
+  {
+    "revision": "cde903f3e3b8984f88f2",
+    "url": "/easyMovies/static/css/2.1097d493.chunk.css"
+  },
+  {
+    "revision": "c3cc22cd44642084cdf9",
+    "url": "/easyMovies/static/css/main.567177f1.chunk.css"
+  },
+  {
+    "revision": "cde903f3e3b8984f88f2",
+    "url": "/easyMovies/static/js/2.f5353556.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/easyMovies/static/js/2.f5353556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3cc22cd44642084cdf9",
+    "url": "/easyMovies/static/js/main.12c43ba4.chunk.js"
+  },
+  {
+    "revision": "7969bf2fb14b0cffdf18",
+    "url": "/easyMovies/static/js/runtime-main.03205701.js"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/easyMovies/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "975ce917e7ac75a277c5115130e69fef",
+    "url": "/easyMovies/static/media/empty_star.975ce917.png"
+  },
+  {
+    "revision": "cfc8572ffbc9cce2661687c4b11bc892",
+    "url": "/easyMovies/static/media/full_star.cfc8572f.png"
+  }
+]);
